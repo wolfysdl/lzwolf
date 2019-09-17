wolfysdl/lzwolf

commit f695a3f2566cbea1a9d65ee811a6a8abdeeb96d7

damage type pain state

src/wl_state.cpp
@@ -1,5 +1,6 @@
 // WL_STATE.C
 
+#include <sstream>
 #include "wl_def.h"
 #include "id_ca.h"
 #include "id_sd.h"
@@ -828,8 +829,23 @@ void DamageActor (AActor *ob, AActor *attacker, unsigned damage, const ClassDef 
 		if (! (ob->flags & FL_ATTACKMODE) )
 			FirstSighting (ob, ob->SeeState);             // put into combat mode
 
+		const Frame *painstate = NULL;
 		if(ob->PainState && pr_damagemobj() < ob->painchance)
-			ob->SetState(ob->PainState);
+			painstate = ob->PainState;
+
+		if (painstate)
+		{
+			const Frame *dmgpainstate = NULL;
+
+			std::stringstream ss;
+			ss << "Pain_" << std::string(damagetype->GetName().GetChars());
+			dmgpainstate = ob->FindState(ss.str().c_str());
+			if (dmgpainstate)
+				painstate = dmgpainstate;
+		}
+
+		if (painstate)
+			ob->SetState(painstate);
 	}
 }
 

commit 30e1539df6ca4480a563cefa7f351afe3006b017

resolve crash introduced by f695a3f

src/wl_state.cpp
@@ -833,7 +833,7 @@ void DamageActor (AActor *ob, AActor *attacker, unsigned damage, const ClassDef 
 		if(ob->PainState && pr_damagemobj() < ob->painchance)
 			painstate = ob->PainState;
 
-		if (painstate)
+		if (painstate && damagetype)
 		{
 			const Frame *dmgpainstate = NULL;
 