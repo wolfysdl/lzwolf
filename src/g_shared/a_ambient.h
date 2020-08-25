45892b85430b19953017eff42d0a09e67809ff26
Linux Wolf <linuxwolf6@gmail.com>
2020-08-25 16:35:51 +1000

    ambient class

---
+++ src/g_shared/a_ambient.h
@@ -0,0 +1,54 @@
+/*
+** a_ambient.h
+**
+**---------------------------------------------------------------------------
+** Copyright 2020 Linux Wolf
+** All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+** 1. Redistributions of source code must retain the above copyright
+**    notice, this list of conditions and the following disclaimer.
+** 2. Redistributions in binary form must reproduce the above copyright
+**    notice, this list of conditions and the following disclaimer in the
+**    documentation and/or other materials provided with the distribution.
+** 3. The name of the author may not be used to endorse or promote products
+**    derived from this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+** IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+** OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+** INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+** NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+** THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**---------------------------------------------------------------------------
+**
+**
+*/
+
+#ifndef __A_AMBIENT_H__
+#define __A_AMBIENT_H__
+
+#include "actor.h"
+
+class AAmbient : public AActor
+{
+	DECLARE_NATIVE_CLASS(Ambient, Actor)
+	HAS_OBJECT_POINTERS
+
+	public:
+		FNameNoInit		ambientsound;
+		int             lastwait;
+
+		void JumpState(const Frame *frame);
+
+		void PlaySound();
+};
+
+#endif
