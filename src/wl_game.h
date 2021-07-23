f593e26e702946a912165cb7565f48a44523494e
Linux Wolf <linuxwolf6@gmail.com>
2021-07-23 23:14:10 +1000

    sfx attenuation turned off by default bug
    Doors are too loud since attenuation was accidentally turned off.

--- src/wl_game.h
+++ src/wl_game.h
@@ -219,7 +219,7 @@ extern struct NewMap_t
 #define PlaySoundLocTile(s,tx,ty)       PlaySoundLocGlobal(s,(((int32_t)(tx) << TILESHIFT) + (1L << (TILESHIFT - 1))),(((int32_t)ty << TILESHIFT) + (1L << (TILESHIFT - 1))),SD_GENERIC)
 #define PlaySoundLocActor(s,ob)         PlaySoundLocGlobal(s,(ob)->x,(ob)->y,SD_GENERIC)
 #define PlaySoundLocActorBoss(s,ob)     PlaySoundLocGlobal(s,(ob)->x,(ob)->y,SD_BOSSWEAPONS)
-void    PlaySoundLocGlobal(const char* s,fixed gx,fixed gy,int chan,unsigned int objId=0,bool looped=false,double attenuation=0.0,double volume=1.0);
+void    PlaySoundLocGlobal(const char* s,fixed gx,fixed gy,int chan,unsigned int objId=0,bool looped=false,double attenuation=1.0,double volume=1.0);
 void UpdateSoundLoc(void);
 
 class SoundIndex;
